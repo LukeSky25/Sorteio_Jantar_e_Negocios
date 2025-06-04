a236dd8714bf5bbdc02dfeeeae7dbdcd67be541a
LukeSky25 <lucasskywalkerodrigues@gmail.com>
2025-06-03 21:24:31 -0300

    Ajustes na Estilização além da finalização da função de busca e escrita da lista de nomes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import axios from "axios";
+
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa6";
 
 import logo from "./assets/logo.png";
 import play from "./assets/play.jpg";
@@ -10,29 +12,86 @@ function App() {
   const [names, setNames] = useState([]);
   const [drawer_n, setDrawer_n] = useState([]);
   const [quant, setQuant] = useState(1);
+  const [isVisible, setIsVisible] = useState(true);
+
+  const textAreaRef = useRef(null);
 
   useEffect(() => {
-    axios
-      .get("http://localhost:3001/arquivo/test2.json")
-      .then((res) => setNames(res.data))
-      .catch((err) => console.log(err));
+    getList();
   }, []);
 
-  const name_drawer = () => {
-    if (names.length >= quant) {
-      const copiaNomes = [...names];
-      const sorteados = [];
+  const reset = async () => {
+    try {
+      const res = await axios.get("http://localhost:3001/escrever/lista.txt");
 
-      for (let i = 0; i < quant; i++) {
-        const index = Math.floor(Math.random() * copiaNomes.length);
-        sorteados.push(copiaNomes[index].nome);
-        copiaNomes.splice(index, 1);
-      }
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const name_drawer = async () => {
+    try {
+      const res = await axios.get(
+        `http://localhost:3001/sortear/nomes.json/${quant}`
+      );
+
+      const sorteados = res.data.sorteados;
 
       setDrawer_n(sorteados);
-    } else {
-      alert(`É necessário pelo menos ${quant} nomes para o sorteio.`);
+
+      const novosNomes = await axios.get(
+        "http://localhost:3001/arquivo/nomes.json"
+      );
+      setNames(novosNomes.data);
+      console.log(sorteados);
+    } catch (error) {
+      alert("Sorteio Finalizado!");
+      console.log(error);
     }
+  };
+
+  const getList = async () => {
+    try {
+      const res = await axios.get("http://localhost:3001/lista");
+
+      if (textAreaRef.current) {
+        if (Array.isArray(res.data)) {
+          textAreaRef.current.value = res.data.join("\n");
+        } else {
+          textAreaRef.current.value = res.data;
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const writeList = async (e) => {
+    try {
+      if (e.key === "Enter") {
+        const textArea = document.querySelector("#list");
+        const value = textArea.value;
+        const lines = value.split("\n").filter(Boolean);
+
+        const res = await axios.post(
+          "http://localhost:3001/lista/lista.txt",
+          lines
+        );
+
+        console.log("Lista enviada com sucesso:", lines);
+
+        reset();
+
+        return res;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
   };
 
   return (
@@ -41,14 +100,14 @@ function App() {
         <main className="container">
           <div className="top_bar">
             <div className="header">
-              <img src={logo} alt="Logo Jantar & Negócios" />
+              <img src={logo} alt="Logo Jantar & Negócios" onClick={reset} />
 
-              <h1>N°25</h1>
+              <h1 className="edition">Edição Nº211 - El Uruguayo</h1>
 
               <div className="form_qtd">
                 <input
                   id="qtd"
-                  type="text"
+                  type="number"
                   min={1}
                   max={names.length}
                   value={quant}
@@ -71,6 +130,20 @@ function App() {
           <button className="drawer_b" onClick={name_drawer}>
             <img src={play} alt="Player" />
           </button>
+
+          <div className="form_names">
+            <textarea
+              ref={textAreaRef}
+              className={`names_${isVisible ? "visible" : "invisible"}`}
+              id="list"
+              type="text"
+              onLoad={getList}
+              onKeyDown={writeList}
+            />
+            <button id="hiding_b" onClick={toggleVisibility}>
+              {isVisible ? <FaRegEye size={20} /> : <FaRegEyeSlash size={20} />}
+            </button>
+          </div>
         </main>
       </section>
     </>
